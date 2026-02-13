28c71a7750fa52f92d6ce88bf013326984b0cabb
Majid Tamboli <169020141+majidtamboli45@users.noreply.github.com>
2026-02-13 12:35:31 +0530

    Add files via upload
    Permutation in String:
    This program checks whether any permutation of string s1 exists as a substring in string s2. It uses character frequency arrays and a sliding window approach to compare each possible substring of s2 with s1, returning true if a matching permutation is found.

---
+++ permutation.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isFreqSame(int freq1[], int freq2[]) {
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        int freq[26] = {0};
+
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1[i] - 'a']++;
+        }
+
+        int windSize = s1.length();
+
+        for (int i = 0; i < s2.length(); i++) {
+            int windIdx = 0, idx = i;
+            int windFreq[26] = {0};
+
+            while (windIdx < windSize && idx < s2.length()) {
+                windFreq[s2[idx] - 'a']++;
+                windIdx++;
+                idx++;
+            }
+
+            if (isFreqSame(freq, windFreq)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    string s1, s2;
+    cout << "Enter string s1: ";
+    cin >> s1;
+    cout << "Enter string s2: ";
+    cin >> s2;
+
+    if (sol.checkInclusion(s1, s2)) {
+        cout << "True: A permutation of s1 exists in s2." << endl;
+    } else {
+        cout << "False: No permutation of s1 exists in s2." << endl;
+    }
+
+    return 0;
+}
